b0cf628aeaac9e20278c95016887e3a012aa4feb
Jason Lee <jasonlee1054@gmail.com>
2017-01-11 11:06:49 -0800

    Added myself to the array of students

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 
 include_once('Student.php');
 
+// Initializing an empty array of students
 $students = array();
 
+// Creating the first student
 $first = new Student();
 $first->surname = "Doe";
 $first->first_name = "John";
@@ -18,8 +20,10 @@ $first->add_email('work','jdoe@mcdonalds.com');
 $first->add_grade(65);
 $first->add_grade(75);
 $first->add_grade(55);
+// Adding first student to the array of students
 $students['j123'] = $first;
 
+// Creating the second student
 $second = new Student();
 $second->surname = "Einstein";
 $second->first_name = "Albert";
@@ -29,9 +33,23 @@ $second->add_email('work2','albert@physics.mit.edu');
 $second->add_grade(95);
 $second->add_grade(80);
 $second->add_grade(50);
+// Adding second student to the array of students
 $students['a456'] = $second;
 
+// Creating me
+$third = new Student();
+$third->surname = "Lee";
+$third->first_name = "Jason";
+$third->add_email('home', 'jasonlee1054@gmail.com');
+$third->add_grade(100); // COMP4711 Lab mark
+$third->add_grade(100); // COMP4711 Assignment mark
+$third->add_grade(100); // COMP4711 Exam mark
+// Adding me to the array of students
+$students['a009'] = $third;
+
+// Sorts the array of students
 ksort($students);
 
-foreach($students as $student)
+// Looping through students and displaying their info
+foreach ($students as $student)
     echo $student->toString();
