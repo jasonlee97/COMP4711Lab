bbb2f48c49d20a3a052b136d467a841eb02a8d9f
Jason Lee <jasonlee1054@gmail.com>
2017-01-11 11:09:31 -0800

    Comments added

--- Student.php
+++ Student.php
@@ -7,6 +7,9 @@
  * Time: 10:51 AM
  */
 class Student {
+    /**
+     * Student constructor.
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -14,14 +17,27 @@ class Student {
         $this->grades = array();
     }
 
+    /**
+     * Adds an email for the student
+     * @param $which
+     * @param $address
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Adds a grade for the student
+     * @param $grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /**
+     * Compute average grade of the student
+     * @return float|int
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -29,12 +45,16 @@ class Student {
         return $total / count($this->grades);
     }
 
+    /**
+     * Display the students information
+     * @return string
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' ('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
-            $result .= $which . ': '. $what. "\n";
+        $result .= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what)
+            $result .= $which . ': ' . $what . "\n";
         $result .= "\n";
-        return '<pre>'.$result.'</pre>';
+        return '<pre>' . $result . '</pre>';
     }
 }
